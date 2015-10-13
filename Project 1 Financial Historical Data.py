d838fd2431e8bcda5d7e27f74737a2c985ccc008
Arif <arifyali@berkeley.edugit config --global user.email arifyali@berkeley.edu>
2015-10-13 13:14:02 -0400

    Limited the amount of Data by only including everything from 2011 onwards and added two features

--- Project 1 Financial Historical Data.py
+++ Project 1 Financial Historical Data.py
@@ -10,94 +10,126 @@ import pandas
 import csv
 
 
-# In[2]:
+# In[41]:
 
 dJIndustial = pandas.read_csv("dowjonesIA.csv")
 dJIndustialTickers = (dJIndustial['Ticker']).tolist()
 
 
-# In[3]:
+# In[42]:
 
 url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+dJIndustialTickers[0]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
 data = pandas.read_csv(url)
+#http://stackoverflow.com/questions/26763344/convert-pandas-column-to-datetime
+#I had to subset data by date, so I needed to convert DataFrame column to Dates
+data['Date']= pandas.to_datetime(data['Date'])
+#I needed to create a datetime that Pandas would recognize so under Time Stamps I found it 
+#http://pandas.pydata.org/pandas-docs/stable/timeseries.html
+data = data.loc[(data.Date >pandas.datetime(2011,1,1))]
 data['ticker'] = dJIndustialTickers[0] 
+data['price change'] = data['Close'] - data['Open']
+data['percentage change'] = data['price change']/data['Open']
 
 
-# In[4]:
+# In[39]:
 
-for ticker in dJIndustialTickers[1:]:
+
+
+
+# In[43]:
+
+for ticker in dJIndustialTickers[1:(len(dJIndustialTickers)-1)]:
     url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+ticker+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
     company = pandas.read_csv(url)
-    company['ticker'] = ticker 
+    company['Date']= pandas.to_datetime(company['Date'])
+    company = company.loc[(company.Date >pandas.datetime(2011,1,1))]
+    company['ticker'] = ticker
+    company['price change'] = company['Open'] - company['Close']
+    company['percentage change'] = company['price change']/company['Open']
+    print(ticker)
     data = data.append(company)
 
 
-# In[5]:
+# In[44]:
 
 data.to_csv("dJIndustialHistoricalData.csv")
 
 
-# In[6]:
+# In[3]:
 
 sNP500 = pandas.read_csv("SP500.csv")
 sNP500Tickers = sNP500['Ticker']
 sNP500Sector = sNP500['Sector']
 
 
-# In[7]:
+# In[46]:
 
 #did sNP500Tickers[0] first
 url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[0]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
 data = pandas.read_csv(url)
+data['Date']= pandas.to_datetime(data['Date'])
+data = data.loc[(data.Date >pandas.datetime(2011,1,1))]
 data['ticker'] = sNP500Tickers[0] 
-data['Sector'] = sNP500Sector[0] 
+data['Sector'] = sNP500Sector[0]
+data['price change'] = data['Close'] - data['Open']
+data['percentage change'] = data['price change']/data['Open']
 
 
-# In[8]:
+# In[47]:
 
 for i in range(1, 250):
     url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[i]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
     try:
         company = pandas.read_csv(url)
+        company['Date']= pandas.to_datetime(company['Date'])
+        company = company.loc[(company.Date >pandas.datetime(2011,1,1))]
         company['ticker'] = sNP500Tickers[i] 
-        company['Sector'] = sNP500Sector[i] 
+        company['Sector'] = sNP500Sector[i]
+        company['price change'] = company['Open'] - company['Close']
+        company['percentage change'] = company['price change']/company['Open']
         data = data.append(company)
     except:
         print(sNP500Tickers[i])
 
 
-# In[9]:
+# In[48]:
 
 data.to_csv("SP500HistoricalDataPart1.csv")
 
 
-# In[10]:
+# In[4]:
 
 url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[250]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
 data = pandas.read_csv(url)
+data['Date']= pandas.to_datetime(data['Date'])
+data = data.loc[(data.Date >pandas.datetime(2011,1,1))]
 data['ticker'] = sNP500Tickers[0] 
-data['Sector'] = sNP500Sector[0] 
+data['Sector'] = sNP500Sector[0]
 
 
-# In[19]:
+# In[5]:
 
 for i in range(251, (len(sNP500)-1)):
     url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[i]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
     try:
         company = pandas.read_csv(url)
+        company['Date']= pandas.to_datetime(company['Date'])
+        company = company.loc[(company.Date >pandas.datetime(2011,1,1))]
         company['ticker'] = sNP500Tickers[i] 
-        company['Sector'] = sNP500Sector[i] 
+        company['Sector'] = sNP500Sector[i]
+        company['price change'] = company['Open'] - company['Close']
+        company['percentage change'] = company['price change']/company['Open']
         data = data.append(company)
     except:
         print(sNP500Tickers[i])
 
 
-# In[20]:
+# In[6]:
 
 data.to_csv("SP500HistoricalDataPart2.csv")
 
 
-# In[27]:
+# In[ ]:
 
 #For The NASDAQ Financial 100 Index, I found a list of certain stocks from as of 10/12/15
 #https://indexes.nasdaqomx.com/Index/Weighting/IXF
