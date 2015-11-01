c6479572ac0e0f2e27f4e001270d258e40a77216
Hongkai Wu <hw271@Hongkais-MacBook-Pro.local>
2015-11-01 16:53:41 -0500

    change structure

---
+++ part1_scrape_data/Data Scraping and Project 1 related scripts/GetNYTimes2014ElectionResults.py
@@ -0,0 +1,169 @@
+# In accordance with the class policies and Georgetown's Honor Code,
+# I certify that, with the exceptions of the lecture notes and those
+# items noted below, this work is my own. I know that I can speak to
+# others about my code, but I cannot share the code itself.
+# If I received any help, I have noted it
+
+print("Begin program: GetNYTimes2014ElectionResults.py") #Indicate to user that program has begun.
+
+#Load packages
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+#List of state names for use while looping - lowercase as they appear in NYTimes url.
+#Alphabetical order.
+stateNameList = ["alabama",        "alaska",        "arizona",
+                 "arkansas",       "california",    "colorado",
+                 "connecticut",    "delaware",      "florida",
+                 "georgia",        "hawaii",        "idaho",
+                 "illinois",       "indiana",       "iowa",
+                 "kansas",         "kentucky",      "louisiana",
+                 "maine",          "maryland",      "massachusetts",
+                 "michigan",       "minnesota",     "mississippi",
+                 "missouri",       "montana",       "nebraska",
+                 "nevada",         "new-hampshire", "new-jersey",
+                 "new-mexico",     "new-york",      "north-carolina",
+                 "north-dakota",   "ohio",          "oklahoma",
+                 "oregon",         "pennsylvania",  "rhode-island",
+                 "south-carolina", "south-dakota",  "tennessee",
+                 "texas",          "utah",          "vermont",
+                 "virginia",       "washington",    "west-virginia",
+                 "wisconsin",      "wyoming"]
+
+#List of state abbreviations - also for use while looping.
+#Order matches state names.
+stateAbbrevList = ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl",
+                   "ga", "hi", "id", "il" ,"in", "ia", "ks", "ky", "la",
+                   "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne",
+                   "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok",
+                   "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt",
+                   "va", "wa", "wv", "wi", "wy"]
+
+#Output csv that will contain election data.
+#Will contain one row per election.
+csvName = "NYTimes2014elections.csv"
+
+#Define a procedure for extracting table information from a soup.
+def extractRaceData(raceSoup, header, electionWriter):
+    #raceSoup - string containing soup output for a given race.
+    #header - list of two elements, first element - state, second element - race e.g. Senate.
+    #electionWriter - csv.writer to use to write output.
+    
+    data = []                          #Initialize data list to empty
+    td = raceSoup.find_all("td")       #Find all table entries
+    datastring = "header[0],header[1]" #datastring will contain all output to be written.
+    noContest="no"                     #Flag indicating whether or not there were opponents in the election.
+                                       #The flag is dynamically set to "yes" based on soup.
+    
+    for i in range (0,len(td)):        #Loop through all table entries
+        
+        currData = td[i].text.strip()         #Pull out table value
+        currData = currData.replace("\n","")  #Some table entries had lots of newline characters - Remove them.
+                                              #I have used this strategy before in other languages,
+                                              #but for the Python syntax I googled http://www.tutorialspoint.com/python/string_replace.htm
+        currData = " ".join(currData.split()) #Reduces white space to a single white space between elements.
+                                              #Found this technique on stack overflow:
+                                              #http://stackoverflow.com/questions/6496884/compress-whitespaces-in-string
+                                              #Verified understanding of its use from python:
+                                              #http://www.tutorialspoint.com/python/string_join.htm
+        
+        if currData[-11:]=="Uncontested":     #If the current candidate is "Uncontested" currData will contain a name and "Unconctested".
+                                              #Apply special process.  Indexing strategy used inspired by stackoverflow:
+                                              #http://stackoverflow.com/questions/663171/is-there-a-way-to-substring-a-string-in-python
+            noContest = "yes"                 #Flip no contest flag.
+            currData = currData[:-11]         #Strip "Uncontested" away from the name of the candidate.
+            
+        if currData!="SHOW ALL HIDE":         #Sometimes this would get pulled with the soup and appear after all candidates in an election.
+                                              #If it does, don't save that information.
+            data.append(currData)                              #Append current table field info to the data variable
+            datastring = datastring + ", data[" + str(i) + "]" #Add this new element of the data variable
+                                                               #to the string that will be evaluated by the writer.
+                                                               #Found this numeric-string conversion strategy on stack overflow:
+                                                               #http://stackoverflow.com/questions/961632/converting-integer-to-string-in-python
+            
+            if i==(len(td)-1) and noContest=="yes":            #Continue special processing in "Unconctested" elections.
+                
+                #Loop through the next two fields (Votes and Votes Percentage) which will be empty,
+                #then enter Uncontested in the "Candidate2" Column.
+                for j in range (0,2):                          
+                    data.append("")
+                    nextI = i + 1 + j
+                    datastring = datastring + ", data[" + str(nextI) + "]"
+                    
+                data.append("Uncontested")
+                nextI = nextI + 1
+                datastring = datastring + ", data[" + str(nextI) + "]"
+                
+    electionWriter.writerow(eval(datastring)) #Write data to csv. Since the number of entries output to any given row varied by election,
+                                              #I wasn't sure of the best way to parameterize the output.  A string seemed straight forward, 
+                                              #but then I had to find a way to force its contents to resolve. After googling I found the eval
+                                              #function that I used: https://docs.python.org/3/library/functions.html#eval
+
+#Open a csv file to which output will be written.
+with open(csvName,"w", newline="", encoding="utf8") as csvfile:
+    electionWriter = csv.writer(csvfile,delimiter=",")
+    
+    #Write header row to the file.
+    #Max number of candidates for any individual election determined by trial and error.
+    electionWriter.writerow (["State", "CongressionalMembership",
+                              "Candidate1",  "PartyAffiliation1",  "Votes1",  "VotePercent1",
+                              "Candidate2",  "PartyAffiliation2",  "Votes2",  "VotePercent2",
+                              "Candidate3",  "PartyAffiliation3",  "Votes3",  "VotePercent3",
+                              "Candidate4",  "PartyAffiliation4",  "Votes4",  "VotePercent4",
+                              "Candidate5",  "PartyAffiliation5",  "Votes5",  "VotePercent5",
+                              "Candidate6",  "PartyAffiliation6",  "Votes6",  "VotePercent6",
+                              "Candidate7",  "PartyAffiliation7",  "Votes7",  "VotePercent7",
+                              "Candidate8",  "PartyAffiliation8",  "Votes8",  "VotePercent8",
+                              "Candidate9",  "PartyAffiliation9",  "Votes9",  "VotePercent9",
+                              "Candidate10", "PartyAffiliation10", "Votes10", "VotePercent10",
+                              "Candidate11", "PartyAffiliation11", "Votes11", "VotePercent11",
+                              "Candidate12", "PartyAffiliation12", "Votes12", "VotePercent12"])
+
+    #Loop through all 50 states
+    for stateNum in range (0,50):
+        print("Current state =", stateNameList[stateNum]) #Print state current under analysis for purposes
+                                                          #of debugging and to provide confidence the program
+                                                          #is still running to user.
+        
+        url = "http://elections.nytimes.com/2014/" + stateNameList[stateNum] + "-elections" #url to soup for this state.
+        page = requests.get(url)                                                            
+        soup = BeautifulSoup(page.text, "lxml")
+
+        for raceType in ["senate", "senateSpecialii", "senateSpecialiii", "house"]: #There are four types of race to pull for 2014 elections.
+            #By raceType, set the header (which will become the first two columns of data in the output),
+            #and find the section of soup corresponding to that election.  This is necessary to avoid pulling state-level races.
+            #Each race has a section heading identifiable in the soup.
+            
+            if raceType=="senate":
+                header = [stateNameList[stateNum], "Senate"]
+                raceSoup = soup.find(id="race-" + stateAbbrevList[stateNum] + "-senate-class-ii-2014-general")
+                
+            elif raceType in ["senateSpecialii", "senateSpecialiii"]: #Special elections are seats that wouldn't normally be in this election cycle.
+                header = [stateNameList[stateNum], "Senate Special"]
+                
+                if raceType=="senateSpecialii":
+                    raceSoup = soup.find(id="race-" + stateAbbrevList[stateNum] + "-senate-class-ii-2014-special-general")
+                    
+                elif raceType=="senateSpecialiii":
+                    raceSoup = soup.find(id="race-" + stateAbbrevList[stateNum] + "-senate-class-iii-2014-general")
+                    
+            elif raceType=="house":
+                houseDistrictNum = 1 #There are different numbers of house districts per state.
+                header = [stateNameList[stateNum], "House District " + str(houseDistrictNum)]
+                #Note the raceSoup for the house districts are based on state AND district number.
+                raceSoup = soup.find(id="race-" + stateAbbrevList[stateNum] + "-house-district-" + str(houseDistrictNum) + "-2014-general")
+
+            if raceType in ["senate", "senateSpecialii", "senateSpecialiii"] and raceSoup!=None: #Some states may not have had a senate race.
+                extractRaceData(raceSoup, header, electionWriter)
+                
+            elif raceType=="house":
+                
+                while raceSoup!=None: #Loop through house district soups until the next district is not found - you have completed the last district.
+                    
+                    extractRaceData(raceSoup, header, electionWriter)
+                    houseDistrictNum = houseDistrictNum + 1 #Iterate house district
+                    header = [stateNameList[stateNum], "House District " + str(houseDistrictNum)]
+                    raceSoup = soup.find(id="race-" + stateAbbrevList[stateNum] + "-house-district-" + str(houseDistrictNum) + "-2014-general")
+
+print("End program: GetNYTimes2014ElectionResults.py") #Indicate to user program is finished running.
---
+++ part1_scrape_data/Data Scraping and Project 1 related scripts/Project 1 Financial Historical Data.py
@@ -0,0 +1,172 @@
+
+# coding: utf-8
+
+# In[10]:
+
+# In accordance with the class policies and Georgetown's Honor Code,
+# I certify that, with the exceptions of the lecture notes and those
+# items noted below, this work is my own. I know that I can speak to
+# others about my code, but I cannot share the code itself.
+# If I received any help, I have noted it
+
+import json#I ended up on needing this packages after finding out the API can output CSVs
+import requests#I ended up on needing this packages after finding out the API can output CSVs
+import urllib2#I ended up on needing this packages after finding out the API can output CSVs
+import pandas
+import csv#I thought I might need it
+
+
+# In[11]:
+
+#Quandl (the website used) required I use tickers and provided a csv with the tickers
+dJIndustial = pandas.read_csv("dowjonesIA.csv")
+##In hindsight, dJIndustial['Ticker'] would have been good enough
+dJIndustialTickers = (dJIndustial['Ticker']).tolist()
+
+
+# In[12]:
+
+##I needed to set a base data case for my loop, therefore I look the first stock to set up the Pandas DataFrame
+##The URL is set up based on the API info from Quandl. 
+url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+dJIndustialTickers[0]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+data = pandas.read_csv(url)
+#http://stackoverflow.com/questions/26763344/convert-pandas-column-to-datetime
+#I had to subset data by date, so I needed to convert DataFrame column to Dates
+data['Date']= pandas.to_datetime(data['Date'])
+#I needed to create a datetime that Pandas would recognize so under Time Stamps I found it 
+#http://pandas.pydata.org/pandas-docs/stable/timeseries.html
+#We were able to obtain election data from 2004, so I grabs corresponding stock data
+data = data.loc[(data.Date >pandas.datetime(2004,1,1))]
+#good way of knowing the companies of data Im collecting is using tickers to identify
+data['ticker'] = dJIndustialTickers[0] 
+#feature 1
+data['price change'] = data['Close'] - data['Open']
+#feature 2
+data['percentage change'] = data['price change']/data['Open']
+#feature 3
+data['Opening Total Value'] = data['Open']*data['Volume']
+#feature 4
+data['Closing Total Value'] = data['Close']*data['Volume']
+
+
+# In[9]:
+
+#In reality, I should have made this a function, but I was a bit overworked
+
+
+# In[13]:
+
+#For Loop, basically every thing from the base.
+for ticker in dJIndustialTickers[1:(len(dJIndustialTickers)-1)]:
+    url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+ticker+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+    company = pandas.read_csv(url)
+    company['Date']= pandas.to_datetime(company['Date'])
+    company = company.loc[(company.Date >pandas.datetime(2004,1,1))]
+    company['ticker'] = ticker
+    company['price change'] = company['Open'] - company['Close']
+    company['percentage change'] = company['price change']/company['Open']
+    company['Opening Total Value'] = company['Open']*company['Volume']
+    company['Closing Total Value'] = company['Close']*company['Volume']
+    print(ticker)
+    data = data.append(company)
+
+
+# In[14]:
+
+data.to_csv("dJIndustialHistoricalData.csv")
+
+
+# In[2]:
+
+#followed through on hindsight 
+sNP500 = pandas.read_csv("SP500.csv")
+sNP500Tickers = sNP500['Ticker']
+#SP 500 had sector in the quandl provided csv, so I included it
+sNP500Sector = sNP500['Sector']
+
+
+# In[3]:
+
+#did sNP500Tickers[0] first, as part of the base idea
+url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[0]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+data = pandas.read_csv(url)
+data['Date']= pandas.to_datetime(data['Date'])
+data = data.loc[(data.Date >pandas.datetime(2004,1,1))]
+data['ticker'] = sNP500Tickers[0] 
+data['Sector'] = sNP500Sector[0]
+data['price change'] = data['Close'] - data['Open']
+data['percentage change'] = data['price change']/data['Open']
+data['Opening Total Value'] = data['Open']*data['Volume']
+data['Closing Total Value'] = data['Close']*data['Volume']
+
+
+# In[4]:
+
+#I kept getting strange no response call errors so I limited the range
+for i in range(1, 250):
+    url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[i]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+    #ran into errors when certain stock data was taken out, so I added try 
+    #so for loop wouldn't break
+    try:
+        company = pandas.read_csv(url)
+        company['Date']= pandas.to_datetime(company['Date'])
+        company = company.loc[(company.Date >pandas.datetime(2004,1,1))]
+        company['ticker'] = sNP500Tickers[i] 
+        company['Sector'] = sNP500Sector[i]
+        company['price change'] = company['Open'] - company['Close']
+        company['percentage change'] = company['price change']/company['Open']
+        company['Opening Total Value'] = company['Open']*company['Volume']
+        company['Closing Total Value'] = company['Close']*company['Volume']
+
+        data = data.append(company)
+    except:
+        #I wanted to know what and how many stocks failed
+        print(sNP500Tickers[i])
+
+
+# In[5]:
+
+data.to_csv("SP500HistoricalDataPart1.csv")
+
+
+# In[6]:
+
+#Another For loop base
+url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[250]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+data = pandas.read_csv(url)
+data['Date']= pandas.to_datetime(data['Date'])
+data = data.loc[(data.Date >pandas.datetime(2004,1,1))]
+data['ticker'] = sNP500Tickers[0] 
+data['Sector'] = sNP500Sector[0]
+data['price change'] = data['Close'] - data['Open']
+data['percentage change'] = data['price change']/data['Open']
+data['Opening Total Value'] = data['Open']*data['Volume']
+data['Closing Total Value'] = data['Close']*data['Volume']
+
+
+# In[7]:
+
+##picked up remaining S&P 500
+for i in range(251, (len(sNP500)-1)):
+    url = 'https://www.quandl.com/api/v3/datasets/YAHOO/'+sNP500Tickers[i]+'.csv?api_key=p3bC2cMj_FbQGSDDjs4y'
+    try:
+        company = pandas.read_csv(url)
+        company['Date']= pandas.to_datetime(company['Date'])
+        company = company.loc[(company.Date >pandas.datetime(2004,1,1))]
+        company['ticker'] = sNP500Tickers[i] 
+        company['Sector'] = sNP500Sector[i]
+        company['price change'] = company['Open'] - company['Close']
+        company['percentage change'] = company['price change']/company['Open']
+        company['Opening Total Value'] = company['Open']*company['Volume']
+        company['Closing Total Value'] = company['Close']*company['Volume']
+
+        data = data.append(company)
+    except:
+        print(sNP500Tickers[i])
+        #after finishing, it turns out only stock I couldn't get was NAVI
+
+
+# In[8]:
+
+data.to_csv("SP500HistoricalDataPart2.csv")
+
