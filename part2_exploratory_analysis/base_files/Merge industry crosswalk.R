989ce220ecd60ea18c0c5e67005c0c7fa0c355ce
joshuaeitan <joshuakaplan@Joshuas-MacBook-Air.local>
2015-11-12 15:55:02 -0500

    nothing important

--- part2_exploratory_analysis/base_files/Merge industry crosswalk.R
+++ part2_exploratory_analysis/base_files/Merge industry crosswalk.R
@@ -2,4 +2,7 @@
 library(RSQLite)
 names(Industry.Crosswalk) = c("INDUSTRY", "PRIMARY INDUSTRY", "SECONDARY INDUSTRY 1", "SECONDARY INDUSTRY 2", "SECONDARY INDUSTRY 3")
 OpenSecretsFECIndustry = merge(OpenSecretsFECmerged,Industry.Crosswalk,by="INDUSTRY",all=TRUE)
+OpenSecretsFECIndustry = OpenSecretsFECIndustry[,c(1,3:ncol(OpenSecretsFECIndustry))]
+names(OpenSecretsFECIndustry)[1] = "OPENSECRETS INDUSTRY"
+
 write.csv(OpenSecretsFECIndustry,file = "OpenSecretsFECIndustry")
