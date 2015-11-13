f23cf2f9bc11f8c404e57c4c242d81cfb4b8d6cd
Arif Ali <arifyuali@gmail.com>
2015-11-13 18:24:51 -0500

    added t test framework

--- part2_exploratory_analysis/hypothesis_testing/Parametric Statistical Tests/Linear Regression Model.R
+++ part2_exploratory_analysis/hypothesis_testing/Parametric Statistical Tests/Linear Regression Model.R
@@ -1,10 +1,13 @@
 ##### Logistic Model ####
+# Please change setwd to wherever "part2_exploratory_analysis" is
+setwd("~/Documents/Analytics 501 Fall 2015/50-percent-Chance-of-Awesome/part2_exploratory_analysis/")
 political_data = read.csv("OpenSecretsFECIndustry.csv")
 
 # Null Hypothesis: There is no linear relation between the percentage of votes and the rank of 
 # industry contributions for the candidates
 
 
+### Figure out how to tell total number of industries supporting each candidate.
 
 Logit_pol_data = political_data[!duplicated(political_data[,c("STATE", "DISTRICT", "CANDIDATE")]),]
 
@@ -17,9 +20,17 @@ Logit_pol_data = political_data[!duplicated(political_data[,c("STATE", "DISTRICT
 
 
 Logit_pol_data$CANDTOTAL = Logit_pol_data$CANDTOTAL/max(Logit_pol_data$CANDTOTAL)
-model <- glm(PERCENT ~CANDTOTAL,data=Logit_pol_data)
+model <- lm(PERCENT ~CANDTOTAL,data=Logit_pol_data)
 summary(model)
 
-# The B1 coefficient is 0.9519, thus we reject the null hypothesis in favor of the alternative 
-# indicating that there is a somewhat linear relation between the Rank of donations by 
-# candidate and the percentage of votes received. 
+# The B1 coefficient is 0.9519 and the p-value is < 2.2e-16, thus we reject the null hypothesis 
+# in favor of the alternative indicating that there is a somewhat linear relation between the 
+# Rank of donations by candidate and the percentage of votes received. 
+
+# t-test
+# Null Hypothesis: There is no difference between the amount of money
+# an incumbent receives vs what a challenger receives.
+Logit_pol_data = political_data[!duplicated(political_data[,c("STATE", "DISTRICT", "CANDIDATE")]),]
+
+pol_t_test = t.test(Logit_pol_data$CANDTOTAL[Logit_pol_data$INCUMBENT==1],
+                    Logit_pol_data$CANDTOTAL[Logit_pol_data$INCUMBENT==0])
