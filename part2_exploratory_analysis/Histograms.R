cd853ded1a2627752a08ebc3a22af206ff02cb8e
timgator <timothyahn@gmail.com>
2015-11-14 15:26:22 -0500

    Added a fifth histogram

--- part2_exploratory_analysis/Histograms.R
+++ part2_exploratory_analysis/Histograms.R
@@ -7,7 +7,6 @@ SP500Monthly <- read.csv("C:/Users/Ahn/Desktop/50-percent-Chance-of-Awesome/part
 Year <- as.numeric(format(as.Date(SP500Monthly$Date),format="%Y"))
 OddYears <- SP500Monthly[Year%%2!=0,]
 EvenYears <- SP500Monthly[Year%%2==0,]
-
 # Create histograms based on election and non-election years and plot them together
 OddHist <- hist(OddYears$PercentChange,breaks=100,plot=F)
 EvenHist <- hist(EvenYears$PercentChange,breaks=50,plot=F)
@@ -42,8 +41,13 @@ hist(IndContrib$Freq,breaks=30,xlim=c(0,5000),ylim=c(0,25),xlab="# of Candidates
 
 
 ### This histogram provides a visual representation of the disparity of industry contribution sizes between
-### winning and losing candidates
-PoldataSPIndustries <- read.csv("C:/Users/Ahn/Desktop/50-percent-Chance-of-Awesome/part2_exploratory_analysis/PoldataSPIndustries.csv", stringsAsFactors=FALSE)
+### winning and losing candidates. The green-shaded bars represent winning candidates and the 
+### black-shaded bars represent the losing candidates.
+### The interquartile range is being used for the purposes of this histogram.
+PoldataSPIndustries <- read.csv("PoldataSPIndustries.csv", stringsAsFactors=FALSE)
+# Find Interquartile Range to use for histogram data. Winners = [25500,157000], Losers = [1500,40500]
+summary(PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==1])
+summary(PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==0])
 Winners <- PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==1 & PoldataSPIndustries$AMOUNT>=25500 & PoldataSPIndustries$AMOUNT<=157000]
 Losers <- PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==0 & PoldataSPIndustries$AMOUNT>=1500 & PoldataSPIndustries$AMOUNT<=40500]
 Windata <- hist(Winners,breaks=30,plot = F)
@@ -51,4 +55,30 @@ Losedata <- hist(Losers,breaks=10,plot = F)
 plot(0,0,type="n",xlim=c(0,160000),ylim=c(0,3000),xlab="Industry Contribution Amounts",ylab="Frequency",main="Frequency of Industry Contributions to Candidates (IQR)")
 plot(Windata,col="green",density=20,add=T)
 plot(Losedata,density=20,angle=135,add=T)
+legend("topright",c("Winning Candidates","Losing Candidates"),fill=c("green","black"),density=20)
+
+
+
+### This histogram shows the frequency of total contribution amounts to each candidate per
+### election, distinguished by winning and losing candidates. The green-shaded bars represent
+### winning candidates and the black-shaded bars represent the losing candidates.
+### The interquartile range is used for the purposes of this histogram.
+PoldataSPIndustries$UniqueCand <- paste(PoldataSPIndustries$YEAR,PoldataSPIndustries$CANDIDATE, sep="-")
+WinnersTotal <- aggregate(PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==1] 
+                  ~ PoldataSPIndustries$UniqueCand[PoldataSPIndustries$WINNER==1],data=PoldataSPIndustries,sum)
+names(WinnersTotal) <- c("Candidate","Amount")
+LosersTotal <- aggregate(PoldataSPIndustries$AMOUNT[PoldataSPIndustries$WINNER==0] 
+                  ~ PoldataSPIndustries$UniqueCand[PoldataSPIndustries$WINNER==0],data=PoldataSPIndustries,sum)
+names(LosersTotal) <- c("Candidate","Amount")
+# Find Interquartile Range to use for histogram data. Winners = [512500,1178000], Losers = [7416,395400]
+summary(WinnersTotal$Amount)
+summary(LosersTotal$Amount)
+WinnersTotal <- WinnersTotal[WinnersTotal$Amount>=512500 & WinnersTotal$Amount<=1178000,]
+LosersTotal <- LosersTotal[LosersTotal$Amount>=7416 & LosersTotal$Amount<=395400,]
+# Create histograms based on winning and losing candidates and plot them together
+WinTotalHist <- hist(WinnersTotal$Amount,breaks=10,plot=F)
+LoseTotalHist <- hist(LosersTotal$Amount,breaks=10,plot=F)
+plot(0,0,type="n",xlim=c(0,1250000),ylim=c(0,800),xlab="Total Contributions per Candidate",ylab="Frequency",main="Frequency of Total Contributions to Candidates per Election (IQR)")
+plot(WinTotalHist,col="green",density=20,add=T)
+plot(LoseTotalHist,density=20,angle=135,add=T)
 legend("topright",c("Winning Candidates","Losing Candidates"),fill=c("green","black"),density=20)
