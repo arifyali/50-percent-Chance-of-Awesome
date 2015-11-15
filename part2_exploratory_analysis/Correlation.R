d36496ee736fe8bafda0d74d65d7a47ce906c226
timgator <timothyahn@gmail.com>
2015-11-14 22:19:16 -0500

    Commented Correlation.R

--- part2_exploratory_analysis/Correlation.R
+++ part2_exploratory_analysis/Correlation.R
@@ -1,11 +1,17 @@
+# Create a new variable to merge data sets by. We will join Year, State, and District to create a new, unique variable.
 PoldataSPIndustries$YrStDis <- paste(PoldataSPIndustries$YEAR,PoldataSPIndustries$STATE,PoldataSPIndustries$DISTRICT,sep="-")
 
+# Total the entire amount of political funding to all candidates by year, state, and district. This will be used
+# to calculate the funding received percentage per candidate in each race.
 RaceTotals <- aggregate(PoldataSPIndustries$AMOUNT ~ PoldataSPIndustries$YEAR + PoldataSPIndustries$STATE + 
                 PoldataSPIndustries$DISTRICT, data=PoldataSPIndustries, sum)
 
-names(RaceTotals) <- c("Year","State","District","Total")
+# Rename variables and create a new variable that is identical in nature to the one created in PoldataSPIndustries
+names(RaceTotals) <- c("Year","State","District","TotalRaceFunds")
 RaceTotals$YrStDis <- paste(RaceTotals$Year,RaceTotals$State,RaceTotals$District,sep="-")
+RaceTotals <- RaceTotals[,4:5]
 
+# Merge PoldataSPIndustries and RaceTotals
 PoldataSPIndustries <- merge(PoldataSPIndustries,RaceTotals[,c("Total","YrStDis")],by="YrStDis")
 PoldataSPIndustries$RaceFundPerc <- NULL
 i <- 1
