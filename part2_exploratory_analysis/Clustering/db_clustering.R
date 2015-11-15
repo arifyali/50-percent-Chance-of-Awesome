a79b5de79497e90d58d62a7ad66f4283b75aecf5
Arif Ali <arifyuali@gmail.com>
2015-11-14 22:12:09 -0500

    dbscan basic template

--- part2_exploratory_analysis/Clustering/db_clustering.R
+++ part2_exploratory_analysis/Clustering/db_clustering.R
@@ -1,9 +1,11 @@
 library(dbscan)
-dataset = read.csv("./breast-cancer.csv")
-dataset = dataset[2:15]
-eps = 0.5
-MinPts = 5
-db = dbscan(dataset, eps=.4, minPts=3)
+dataset = read.csv("PoldataSPIndustriesStockData.csv")
+dataset = dataset[, mapply(is.numeric, dataset)]
+dataset = dataset[complete.cases(dataset),]
+dataset = dataset[sample(nrow(dataset), 5000), c("INDUSTRYPERCENT", "PERCENT", "YrPercentChange")]
+
+db = dbscan(dataset, eps=.4, minPts=5)
 db
 summary(db)
 plot(dataset, col=db$cluster+1L)
+
