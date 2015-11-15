15d3a2d891e3f3ec50d386d43b73c83272e8b2b8
Arif Ali <arifyuali@gmail.com>
2015-11-15 13:04:21 -0500

    Even better Kmeans plots

--- part2_exploratory_analysis/Clustering/kmeans clustering.R
+++ part2_exploratory_analysis/Clustering/kmeans clustering.R
@@ -2,7 +2,7 @@
 # I also decided not to cluster based on any of the percentages
 setwd("~/Documents/Analytics 501 Fall 2015/50-percent-Chance-of-Awesome/part2_exploratory_analysis/")
 political_data= read.csv("PSPI no outliers.csv")
-
+political_data = political_data[order(political_data$INDRANK, decreasing = T),]
 
 # Focusing on the no outlier dataset first
 
@@ -30,7 +30,7 @@ political_centers = kmeans(numeric_political_data, 4)
 
 political_data_with_clusters = cbind(numeric_political_data, cluster = political_centers$cluster)
 
-png("Clustering/kmeans political centers with incumbents vs challengers.png")
+png("Clustering/kmeans political centers with incumbents vs challengers and winners vs losers.png")
 clusplot(political_data_with_clusters, political_data_with_clusters$cluster, 
          color = T, shade = T, labels = 2, lines = 0)
 dev.off()
