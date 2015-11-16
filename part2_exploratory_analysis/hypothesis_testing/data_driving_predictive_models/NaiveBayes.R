25e2704b904b67678c8a4da9a69cfd149928b4cb
Arif Ali <arifyuali@gmail.com>
2015-11-16 17:51:20 -0500

    Naive Bayes Updated

--- part2_exploratory_analysis/hypothesis_testing/data_driving_predictive_models/NaiveBayes.R
+++ part2_exploratory_analysis/hypothesis_testing/data_driving_predictive_models/NaiveBayes.R
@@ -4,69 +4,54 @@
 library(e1071)
 
 #load the dataset
-dataset = read.csv("hypothesis_testing/data_driving_predictive_models/data.csv")
-#attach(Smarket)
+dataset = read.csv("PoldataSPIndustriesStockData no outliers.csv")
+dataset = dataset[order(dataset$INDRANK, decreasing = T),]
+dataset = dataset[!duplicated(dataset[,c("YEAR", "STATE", "DISTRICT", "CANDIDATE")]),]
+
 
 #bin the data
+dataset$WINNER = as.factor(dataset$WINNER)
 
-x = dataset[, 2]
-industryPercentLevel = cut(x, b=6, labels=c(1:6))
-dataset = cbind(dataset, industryPercentLevel)
-
-x = dataset[, 3]
+x = dataset[, "CANDTOTAL"]
 candTotalLevel = cut(x, b=10, labels = c(1:10))
 dataset = cbind(dataset, candTotalLevel)
 
-a1 = dataset[, 11]
-amount1Level = cut(a1, b=10, labels = c(1:10))
-dataset = cbind(dataset, amount1Level)
-
-a2 = dataset[, 11]
-amount2Level = cut(a2, b=10, labels = c(1:10))
-dataset = cbind(dataset, amount2Level)
-
-a3 = dataset[, 11]
-amount3Level = cut(a3, b=10, labels = c(1:10))
-dataset = cbind(dataset, amount3Level)
-
-a4 = dataset[, 11]
-amount4Level = cut(a4, b=10, labels = c(1:10))
-dataset = cbind(dataset, amount4Level)
+x = dataset[, "INDRANK"]
+INDLevel = cut(x, b=12, labels = c(1:12))
+dataset = cbind(dataset, INDLevel)
 
 
-a5 = dataset[, 11]
-amount5Level = cut(a5, b=10, labels = c(1:10))
-dataset = cbind(dataset, amount5Level)
-
-
-dataset = dataset[, c(1,4:10,16:22)]
+dataset = dataset[, c("WINNER", "CANDTOTAL", "candTotalLevel", "INDRANK", "INDLevel")]
 
 #dataset = dataset[,c(1,3:15)]
 #10-folds-accross
-train = sample(1:nrow(dataset),nrow(dataset)*9/10)
-test = -train
-traindata = dataset[train,]
-testdata = dataset[test,]
+for(i in 1:3){
+  train = sample(1:nrow(dataset),nrow(dataset)*2/3)
+  test = -train
+  traindata = dataset[train,]
+  testdata = dataset[test,]
+  
+  #traindata$WINNER is the label. 
+  nb = naiveBayes(traindata$WINNER~., data=traindata)
+  
+  #use naive bayes to do prediction
+  pred = predict(nb, testdata[,-1], type = c("class"))
+  
+  #show the result
+  print(table(pred, testdata$WINNER))
+  
+  library(pROC)
+  testTarget = as.numeric(testdata$WINNER)
+  result = as.numeric(pred)
+  myROC = roc(testTarget,result, 
+              print.auc=TRUE,
+              main="Confidence intervals", percent=TRUE,
+              ci=TRUE,  auc.polygon=TRUE, grid=TRUE, plot=FALSE)
+  jpeg(paste0("hypothesis_testing/data_driving_predictive_models/Naive Bayes ROC validation ", i, ".jpeg", sep=""))
+  plot(myROC)
+  
+  #the performance
+  print(mean(pred==testdata$WINNER))
+  print(var(pred==testdata$WINNER))
 
-#traindata$WINNER is the label. 
-nb = naiveBayes(traindata$WINNER~., data=traindata)
-
-#use naive bayes to do prediction
-pred = predict(nb, testdata,type =c("class"))
-
-#show the result
-table(pred, testdata$WINNER)
-
-library(pROC)
-testTarget = as.numeric(testdata$WINNER)
-result = as.numeric(pred)
-myROC = roc(testTarget,result, 
-            print.auc=TRUE,
-            main="Confidence intervals", percent=TRUE,
-            ci=TRUE,  auc.polygon=TRUE, grid=TRUE, plot=FALSE)
-plot(myROC)
-
-#the performance
-mean(pred==testdata$WINNER)
-var(pred==testdata$WINNER)
-
+}
