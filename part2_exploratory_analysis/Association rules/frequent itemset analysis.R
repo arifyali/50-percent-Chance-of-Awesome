4a8ebb4a9d58327dcd2c2b49b7ac5cdacde1d4f8
joshuaeitan <joshuakaplan@Joshuas-MacBook-Air.local>
2015-11-17 12:26:02 -0500

    removed outliers based on vote percent, instead of vote count

--- part2_exploratory_analysis/Association rules/frequent itemset analysis.R
+++ part2_exploratory_analysis/Association rules/frequent itemset analysis.R
@@ -6,17 +6,21 @@ library(arules)
 # load in dataset without outliers
 PoldataSPIndustries <- read.csv("~/50-percent-Chance-of-Awesome/part2_exploratory_analysis/PSPI no outliers.csv")
 
+
+#
+PoldataSPIndustries$VOTEPERCENTLEVEL = cut(PoldataSPIndustries$PERCENT,4,labels = c("Very Low","Mid-Low","Mid-High","High"))
+
 # convert dataset to type factor
 for (i in 1:ncol(PoldataSPIndustries)){
   PoldataSPIndustries[,i] = as.factor(PoldataSPIndustries[,i])
 }
 
 # select subset of dataset to be used in FIA
-FIAsubset = PoldataSPIndustries[,c(2,3,8,12,15,19,22:24)]
+FIAsubset = PoldataSPIndustries[,c(2,3,8,12,15,19,22,24:25)]
 
 # run association rule mining with Apriori algorithm
 rules= apriori(FIAsubset,parameter = list(maxlen=20, supp=0.4, conf=0.2))
-write(rules,file = "test.01.2.csv",sep = ",",quote=TRUE)
+write(rules,file = "apriorirules.4.2.csv",sep = ",",quote=TRUE)
 rules= apriori(FIAsubset,parameter = list(maxlen=20, supp=0.2, conf=0.2))
 write(rules,file = "apriorirules.2.2.csv",sep = ",",quote = TRUE)
 rules = apriori(FIAsubset,parameter = list(maxlen = 20,supp=.05,conf = 0.2))
